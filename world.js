bdf503176e711953a9b1e5aa12be4fad0a282f02
Tinotenda Mhedziso <129593058+PassionOverPain@users.noreply.github.com>
2024-11-07 19:23:21 +0200

    A bug found and squashed

--- world.js
+++ world.js
@@ -1,46 +1,46 @@
 function createMonster(options) {
-  const template = document.createElement("template");
+	const template = document.createElement("template");
 
-  template.innerHTML = `<div id="enemy${arrEnemies.length}" class="pgCard">
+	template.innerHTML = `<div id="enemy${arrEnemies.length}" class="pgCard">
          <img src="${options.picture}" alt="This is a enemy player"  class="Enemy enemyCard" data-ennumber="${arrEnemies.length}"  id="enemy${arrEnemies.length}img"/>    
           </div>`;
 
-  const monster = template.content.firstElementChild;
-  return monster;
+	const monster = template.content.firstElementChild;
+	return monster;
 }
 function newMonster(Index) {
-  let monsterCon = document.getElementById("monstersCon");
-  const myMonster = createMonster({
-    picture: `Images/${myMonsters[Index].Name}.webp`,
-  });
-  if (arrEnemies.length == 0) {
-    monsterCon.replaceChildren();
-  }
-  if (arrEnemies.length == 5) {
-    monsterCon.style.gridTemplateColumns = `auto auto`;
-  }
-  monsterCon.appendChild(myMonster);
+	let monsterCon = document.getElementById("monstersCon");
+	const myMonster = createMonster({
+		picture: `Images/${myMonsters[Index].Name}.webp`,
+	});
+	if (arrEnemies.length == 0) {
+		monsterCon.replaceChildren();
+	}
+	if (arrEnemies.length == 5) {
+		monsterCon.style.gridTemplateColumns = `auto auto`;
+	}
+	monsterCon.appendChild(myMonster);
 }
 let arrEnemies = [];
 function creationMonster(Index) {
-  if (arrEnemies.length < 10) {
-    let myMonster = new Monster(Index);
-    arrEnemies.push(myMonster);
-  }
+	if (arrEnemies.length < 10) {
+		let myMonster = new Monster(Index);
+		arrEnemies.push(myMonster);
+	}
 }
 class Monster {
-  constructor(Index) {
-    this.Index = Index;
-    this.Hp = myMonsters[Index].Hp;
-    newMonster(Index);
-  }
+	constructor(Index) {
+		this.Index = Index;
+		this.Hp = myMonsters[Index].Hp;
+		newMonster(Index);
+	}
 
-  damageHealth(damage) {
-    this.Hp = this.Hp - damage;
-  }
-  healHealth(points) {
-    Hp = Hp + points;
-  }
+	damageHealth(damage) {
+		this.Hp = this.Hp - damage;
+	}
+	healHealth(points) {
+		Hp = Hp + points;
+	}
 }
 
 const memGameCon = document.querySelector(`.memGameCon`);
@@ -55,154 +55,154 @@ let cheat = false;
 document.querySelector(".Attempts").textContent = attemptsLeft;
 
 function startMemoryGame() {
-  document.querySelector("#memGame").style.display = "block";
-  document.querySelector(".memoryCon").style.display = "block";
-  let playground = document.getElementById("playground");
-  playground.style.justifyContent = "center";
-  if (!cheat) {
-    attemptsLeft = 20;
-  }
-  matchedPairs = 0;
-  document.querySelector(".Attempts").textContent = attemptsLeft;
-  memGameCon.innerHTML = ""; // Clear previous cards
+	document.querySelector("#memGame").style.display = "block";
+	document.querySelector(".memoryCon").style.display = "block";
+	let playground = document.getElementById("playground");
+	playground.style.justifyContent = "center";
+	if (!cheat) {
+		attemptsLeft = 20;
+	}
+	matchedPairs = 0;
+	document.querySelector(".Attempts").textContent = attemptsLeft;
+	memGameCon.innerHTML = ""; // Clear previous cards
 
-  // Fetch card data and initialize the game
-  fetch("./memGame.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data];
-      shuffleCards();
-      generateCards();
-    });
+	// Fetch card data and initialize the game
+	fetch("./memGame.json")
+		.then((res) => res.json())
+		.then((data) => {
+			cards = [...data, ...data];
+			shuffleCards();
+			generateCards();
+		});
 }
 
 function shuffleCards() {
-  let currentIndex = cards.length,
-    randomIndex,
-    temporaryValue;
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = cards[currentIndex];
-    cards[currentIndex] = cards[randomIndex];
-    cards[randomIndex] = temporaryValue;
-  }
+	let currentIndex = cards.length,
+		randomIndex,
+		temporaryValue;
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = cards[currentIndex];
+		cards[currentIndex] = cards[randomIndex];
+		cards[randomIndex] = temporaryValue;
+	}
 }
 
 function generateCards() {
-  for (let card of cards) {
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    cardElement.setAttribute("data-name", card.name);
-    cardElement.innerHTML = ` 
+	for (let card of cards) {
+		const cardElement = document.createElement("div");
+		cardElement.classList.add("card");
+		cardElement.setAttribute("data-name", card.name);
+		cardElement.innerHTML = ` 
         <div class="front">
             <img class="front-image" src="${encodeURI(card.image)}" />
         </div>
         <div class="back"></div>`;
-    memGameCon.appendChild(cardElement);
-    cardElement.addEventListener("click", flipCard);
-  }
+		memGameCon.appendChild(cardElement);
+		cardElement.addEventListener("click", flipCard);
+	}
 }
 
 function flipCard() {
-  if (lockBoard || attemptsLeft === 0) return;
-  if (this === firstCard) return;
+	if (lockBoard || attemptsLeft === 0) return;
+	if (this === firstCard) return;
 
-  this.classList.add("flipped");
+	this.classList.add("flipped");
 
-  if (!firstCard) {
-    firstCard = this;
-    return;
-  } else {
-    secondCard = this;
-    attemptsLeft--;
-    document.querySelector(".Attempts").textContent = attemptsLeft;
+	if (!firstCard) {
+		firstCard = this;
+		return;
+	} else {
+		secondCard = this;
+		attemptsLeft--;
+		document.querySelector(".Attempts").textContent = attemptsLeft;
 
-    checkForMatch();
-    checkGameOver();
-  }
+		checkForMatch();
+		checkGameOver();
+	}
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
-  isMatch ? disableCards() : unflipCards();
+	let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+	isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-  resetBoard();
-  matchedPairs++;
-  if (matchedPairs === 9) {
-    displayModal("Victory Achieved");
-    document.querySelector(`.memoryCon`).style.display = "none";
-    document.querySelector(".memGame").style.display = "none";
-    displayStory();
-  }
+	firstCard.removeEventListener("click", flipCard);
+	secondCard.removeEventListener("click", flipCard);
+	resetBoard();
+	matchedPairs++;
+	if (matchedPairs === 9) {
+		displayModal("Victory Achieved");
+		document.querySelector(`.memoryCon`).style.display = "none";
+		document.querySelector("#memGame").style.display = "none";
+		displayStory();
+	}
 }
 
 function unflipCards() {
-  setTimeout(() => {
-    firstCard.classList.remove("flipped");
-    secondCard.classList.remove("flipped");
-    resetBoard();
-  }, 1000);
+	setTimeout(() => {
+		firstCard.classList.remove("flipped");
+		secondCard.classList.remove("flipped");
+		resetBoard();
+	}, 1000);
 }
 
 function resetBoard() {
-  firstCard = null;
-  secondCard = null;
-  lockBoard = false;
+	firstCard = null;
+	secondCard = null;
+	lockBoard = false;
 }
 
 function checkGameOver() {
-  if (attemptsLeft === 0) {
-    displayModal("Game Over! Try Again.");
-    restartMemGame();
-  }
+	if (attemptsLeft === 0) {
+		displayModal("Game Over! Try Again.");
+		restartMemGame();
+	}
 }
 
 function restartMemGame() {
-  resetBoard();
-  shuffleCards();
-  if (!cheat) {
-    attemptsLeft = 20;
-  }
-  matchedPairs = 0;
-  document.querySelector(".Attempts").textContent = attemptsLeft;
-  memGameCon.innerHTML = "";
-  generateCards();
+	resetBoard();
+	shuffleCards();
+	if (!cheat) {
+		attemptsLeft = 20;
+	}
+	matchedPairs = 0;
+	document.querySelector(".Attempts").textContent = attemptsLeft;
+	memGameCon.innerHTML = "";
+	generateCards();
 }
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.Model Stuff <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,,
 
 function displayModal(text) {
-  const modal = document.getElementById("customModal");
-  const modalText = document.getElementById("modalText");
-  modalText.textContent = text;
-  modal.classList.remove("hidden");
+	const modal = document.getElementById("customModal");
+	const modalText = document.getElementById("modalText");
+	modalText.textContent = text;
+	modal.classList.remove("hidden");
 }
 
 function closeModal() {
-  document.getElementById("customModal").classList.add("hidden");
+	document.getElementById("customModal").classList.add("hidden");
 }
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Battle Events And Effects <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 function triggerAttack(enemyId, gifSrc) {
-  const enemyDiv = document.getElementById(`enemy${enemyId}`);
+	const enemyDiv = document.getElementById(`enemy${enemyId}`);
 
-  // Create an img element for the attack effect
-  const attackEffect = document.createElement("img");
-  attackEffect.src = gifSrc;
-  attackEffect.classList.add("attack-gif");
-  attackEffect.style.width = "400%";
+	// Create an img element for the attack effect
+	const attackEffect = document.createElement("img");
+	attackEffect.src = gifSrc;
+	attackEffect.classList.add("attack-gif");
+	attackEffect.style.width = "400%";
 
-  attackEffect.onload = () => {
-    setTimeout(() => {
-      attackEffect.remove();
-    }, 3000);
-  };
+	attackEffect.onload = () => {
+		setTimeout(() => {
+			attackEffect.remove();
+		}, 3000);
+	};
 
-  enemyDiv.appendChild(attackEffect);
+	enemyDiv.appendChild(attackEffect);
 }
